43d8eb9b496aa1729069b0a212f45fb2c16c655a
udaysai-kandregula <udaysai.kandregula@gmail.com>
2026-03-25 21:06:59 +0530

    53. Print the English alphabet from a to z

---
+++ Simple_Repetition/Alphabets.java
@@ -0,0 +1,9 @@
+// 53. Print the English alphabet from a to z.
+
+class Alphabets{
+    public static void main(String [] args){
+        for(char i = 'a' ; i <= 'z' ; i++){
+            System.out.println(i);
+        }
+    }
+}
