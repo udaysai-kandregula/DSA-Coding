7967b4478f93c92f6a0dfc7d0e24efbcf41c50a8
udaysai-kandregula <udaysai.kandregula@gmail.com>
2026-03-25 19:22:17 +0530

    51. Print all natural numbers from 1 to n.

---
+++ Simple_Repetition/PrintNum.java
@@ -0,0 +1,9 @@
+// 51. Print all natural numbers from 1 to n.
+class PrintNum{
+    public static void main(String [] args){
+        int num = 10;
+        for(int i = 1 ; i <= num ; i++ ){
+            System.out.println(i);
+        }
+    }
+}
