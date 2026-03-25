b780eee2abdcae4a5e46ea87976d065a1fd384cc
udaysai-kandregula <udaysai.kandregula@gmail.com>
2026-03-25 21:07:33 +0530

    54. Print all even numbers between 1 and 100.

---
+++ Simple_Repetition/EvenNum.java
@@ -0,0 +1,11 @@
+// 54. Print all even numbers between 1 and 100.
+
+class EvenNum{
+    public static void main(String [] args){
+        for(int i = 1 ; i <= 100 ; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
