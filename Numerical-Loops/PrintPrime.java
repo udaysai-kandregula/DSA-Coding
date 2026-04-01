9fe885c2aa54190f7fb8d972dbd0d17a143c0fb4
udaysai-kandregula <udaysai.kandregula@gmail.com>
2026-04-01 09:09:27 +0530

    76. Print all Prime numbers between 1 and n.

---
+++ Numerical-Loops/PrintPrime.java
@@ -0,0 +1,26 @@
+// 76. Print all Prime numbers between 1 and n.
+import java.util.Scanner;
+public class PrintPrime{
+    public static int checkprime(int num){
+        for(int i = 2 ; i <= num / 2 ; i++){
+            if(num % i == 0){
+                return 0;
+            }
+        }
+        return num;
+    }
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number ");
+        int n = sc.nextInt();
+        int count=0;
+        for(int i = 2 ; i <= n; i ++){
+            int c = checkprime(i);
+            if(c>0){
+                System.out.print(c + " ");
+                count++;
+            }
+        }
+        System.out.println("\n\ncount of number is : " + count);
+    }
+}
