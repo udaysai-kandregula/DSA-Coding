1108e8370a104434803f9cb6cd886d5310d17318
udaysai-kandregula <udaysai.kandregula@gmail.com>
2026-04-02 06:33:35 +0530

    78. Find prime factors of a number

--- Numerical-Loops/Factors.java
+++ Numerical-Loops/Factors.java
@@ -1,12 +1,26 @@
-// 71. Find all factors of a number.
+// 78. Find prime factors of a number.
+
 class Factors{
-    public static void main(String [] args){
-        int num = 30;
-        for(int i = 2 ; i <= num /2 ; i++ ){
+   static int checkprime(int num){
+    int count =0;
+        for(int i = 2 ; i <= num/2 ; i++){
             if(num % i == 0){
-                System.out.println(i);
+                count++;
             }
         }
-        System.out.println("1 and itself of a nunber is default factors : " + 1  +" & " + num);
+        if(count == 0){
+            return num;
+        }
+        return 0;
+    }
+    public static void main(String [] args){
+       int num = 7429;
+       for(int i = 2 ;  i <= num/2 ; i++){
+        int c = checkprime(i);
+        if(c > 0 && num % c == 0){
+            System.out.println(c);
+        }
+
+       }
     }
 }
