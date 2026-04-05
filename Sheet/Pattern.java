241c7070be05ff7cb4370a8c0f0bdcffc818fb7e
udaysai-kandregula <udaysai.kandregula@gmail.com>
2026-04-05 11:48:19 +0530

    adding rightAngleTrinagle and rightAngledNumberPyramid

--- Sheet/Pattern.java
+++ Sheet/Pattern.java
@@ -1,14 +1,45 @@
+import java.util.Scanner;
 class Pattern{
-    static void box(int num){
+    void box(int num){
         for(int i = 0 ; i < num ; i++){
             for(int j = 0 ; j < num ; j++){
-                System.out.print("x" + " ");
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+    void rightAngleTrinagle(int num){
+        for(int i = 0 ; i < num ; i++){
+            for(int j = 0 ; j <= i ; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+    void rightAngledNumberPyramid(int num){
+        for(int i = 1 ; i <= num ; i++){
+            for(int j = 1 ; j <= i ; j++){
+                System.out.print(j +" ");
             }
             System.out.println();
         }
     }
     public static void main(String [] args){
+        
+        // inputs 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size ");
+        int num = sc.nextInt();
+
         Pattern obj = new Pattern();
-        obj.box(5);
+
+        System.out.println("\nBOX Pattern");
+        obj.box(num);
+
+        System.out.println("\nRight Angle Triangle");
+        obj.rightAngleTrinagle(num);
+
+        System.out.println("\nRight-Angled Number Pyramid");
+        obj.rightAngledNumberPyramid(num);
     }
 }
