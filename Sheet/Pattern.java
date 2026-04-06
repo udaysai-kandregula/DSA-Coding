188ae8c9cde9a66ace9e99c9ec1d83b9f71a19dd
udaysai-kandregula <udaysai.kandregula@gmail.com>
2026-04-06 19:55:36 +0530

    adding Inverted Right Pyramid,Inverted Numbered Right Pyramid,Star Pyramid

--- Sheet/Pattern.java
+++ Sheet/Pattern.java
@@ -25,14 +25,55 @@ class Pattern{
             System.out.println();
         }
     }
-        void rightAngledNumberPyramid2(int num ){
-            for(i = 1 ; i <= num ; i++){
-                for(j = 0 ; j < i ; j++){
-                    System.out.print(i +" ");
-                }
-                System.out.println();
+    void rightAngledNumberPyramid2(int num ){
+        for(i = 1 ; i <= num ; i++){
+            for(j = 0 ; j < i ; j++){
+               System.out.print(i +" ");
             }
+            System.out.println();
+        }
     }
+    void inventedRigthPyramid(int num){
+        for(i = 0 ; i < num ; i++){
+            for(j = 0 ; j < num-i ; j++){
+                System.out.print("*"+ " ");
+            }
+            System.out.println();
+        }
+    }
+    void inventedNumRigthPyramid(int num){
+        for(i = 0 ; i < num ; i++){
+            for(j = 1 ; j <= num-i ; j++){
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+    }
+    void starPyramid(int num){
+        int k = 1,count = 1;
+        for(i = 1 ; i <= num ; i++){
+            for(j = 1 ; j <= num-i ; j++){
+                System.out.print(" ");
+            }
+            for(k = 1 ; k <= count ; k++){
+                System.out.print("*" +"");
+            }
+            count +=2;
+            System.out.println();
+        }  
+        System.out.println("\nModel II\n");
+        for(i = 0 ; i < num ; i++){
+            for(j = 0; j < num-i ; j++){
+                System.out.print(" ");
+            }
+            for(k = 0 ; k < 2*i+1 ; k++){
+                System.out.print("*" +"");
+            }
+            System.out.println();
+        }
+
+    }
+
     public static void main(String [] args){
         
         // inputs 
@@ -42,16 +83,28 @@ class Pattern{
 
         Pattern obj = new Pattern();
 
-        System.out.println("\nBOX Pattern");
+        System.out.println("\nPattern-1:BOX Pattern");
         obj.box(num);
 
-        System.out.println("\nRight Angle Triangle");
+        System.out.println("\nPattern-2:Right Angle Triangle");
         obj.rightAngleTrinagle(num);
 
-        System.out.println("\nRight-Angled Number Pyramid");
+        System.out.println("\nPattern-3:Right-Angled Number Pyramid");
         obj.rightAngledNumberPyramid(num);
         
-        System.out.println("\nRight-Angled Number Pyramid - II");
+        System.out.println("\nPattern-4:Right-Angled Number Pyramid - II");
         obj.rightAngledNumberPyramid2(num);
+
+        System.out.println("\nPattern-5: Inverted Right Pyramid");
+        obj.inventedRigthPyramid(num);
+
+        System.out.println("\nPattern - 6: Inverted Numbered Right Pyramid");
+        obj.inventedNumRigthPyramid(num);
+
+        System.out.println("\nPattern - 7: Star Pyramid");
+        obj.starPyramid(num);
+
+        // System.out.println("_" + *5);
+
     }
 }
