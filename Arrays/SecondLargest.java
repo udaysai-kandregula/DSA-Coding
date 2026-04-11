52a3193bb6744e0f487b5d451b65e71462726188
udaysai-kandregula <udaysai.kandregula@gmail.com>
2026-04-11 12:04:11 +0530

    5. Find the second largest element in an array.

--- Arrays/SecondLargest.java
+++ Arrays/SecondLargest.java
@@ -4,16 +4,18 @@ class SecondLargest{
         int arr [] = {10,20,30,21,2,21,44,54};
         // int i = 0 , j = arr.length-1;
         int small = arr[0];
-        for(int i = 0; i < arr.length-1 ; i++){
-            for(int j = 1; j < arr.length ; j++ )
-            if(arr[i] > arr[j]){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length-1 ; j++ )
+            if(arr[j+1] > arr[j]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
         for(int i = 0 ; i < arr.length ; i++){
         System.out.println(arr[i]);
         }
+        System.out.println("Second largest number is " + arr[1]);
+        
     }
 }
