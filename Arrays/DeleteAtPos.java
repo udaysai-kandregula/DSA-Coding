8a94803825d7bc047905598386624bb5f36bfef3
udaysai-kandregula <udaysai.kandregula@gmail.com>
2026-04-14 15:26:38 +0530

    10. Delete an element from a specific position.

---
+++ Arrays/DeleteAtPos.java
@@ -0,0 +1,14 @@
+class DeleteAtPos{
+    public static void main(String [] args){
+        int pos = 2;
+        int arr [] = {1,2,3,4,5,6,7};
+        int i = pos;
+        while(i < arr.length-1){
+            arr[pos] = arr[pos+1];
+            i++;
+        }
+        for(int j : arr){
+            System.out.println(j);
+        }
+    }
+}
