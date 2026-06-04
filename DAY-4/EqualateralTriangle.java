6e18adc3344cda0a8658c5c4e5f18b5a6c1a8ae5
udaysai-kandregula <udaysai.kandregula@gmail.com>
2026-06-04 21:39:43 +0530

    5. Perimeter of Equilateral Triangle

---
+++ DAY-4/EqualateralTriangle.java
@@ -0,0 +1,16 @@
+// 5. Perimeter of Equilateral Triangle
+
+
+
+import java.util.Scanner;
+class EquilateralTriangle{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of one side here :");
+        int side = sc.nextInt();
+        System.out.println("Area of Equilateral Triangle based on the Side lenght is here : "+ (3 * side ));
+    }
+}
+
+// formula =  3 × side
+
